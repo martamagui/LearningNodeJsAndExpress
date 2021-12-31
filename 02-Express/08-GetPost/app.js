d2fd24be7eeffb24b4daafa031f073f078325fa7
Marta Molina <marta.molina@live.u-tad.com>
2021-12-31 13:42:56 +0100

    Post Form example

---
+++ 02-Express/08-GetPost/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+let { people } = require("../data");
+
+//static assets
+app.use(express.static("./methods-public"));
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json(people);
+});
+//Recoge la info del formulario de index.html
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please, provide credentials");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000");
+});
